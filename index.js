652baab1deafac18e8bbe9f40c4bec79a6d4de03
rafinha24 <144127796+rafinha24@users.noreply.github.com>
2024-04-22 18:17:53 -0300

    confirmo

---
+++ index.js
@@ -0,0 +1,8 @@
+
+        function cadastro(){
+            alert("obrigado por se cadastrar no nosso site");
+        }
+        function recadastro(){
+            alert("Você foi recadrastrado, desculpe por fazer isso denovo");
+        }
+    
